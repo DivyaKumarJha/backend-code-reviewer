68cc897f0291484f3b968f5ad145317c11a880fa
DivyaKumarJha <202351034@iiitvadodara.ac.in>
2025-05-29 10:20:43 +0530

    bs3

--- src/app.js
+++ src/app.js
@@ -4,7 +4,14 @@ const cors = require('cors');
 
 
 const app = express();
-app.use(cors()); // Enable CORS for all routes
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://frontend-code-reviewer.vercel.app'
+];
+
+app.use(cors({
+  origin: allowedOrigins,
+}));
 
 app.use(express.json()); // Middleware to parse JSON bodies
 app.get('/', (req, res) => {
